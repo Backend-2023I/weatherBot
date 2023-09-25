db788b053921329481b1636c43bf8606e81c62a2
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-25 14:35:37 +0500

    get weather data

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import requests
 import json
 
-api_key = '29af92d9f93c8cf795be3ab8bb4f6776'
-city = "Samarkand"
 
-url = f"https://api.openweathermap.org/data/2.5/weather"
-params = {
-    "q":city,
-    "appid": api_key,
-}
+def getWeather(city:str):
+    api_key = '29af92d9f93c8cf795be3ab8bb4f6776'
 
-r = requests.get(url, params=params)
-data = r.json()
+    url = f"https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q":city,
+        "appid": api_key,
+    }
 
-f = open('data.json', 'w')
-json.dump(data, f, indent=4)
-f.close()
+    r = requests.get(url, params=params)
+    data = r.json()
+    return data
